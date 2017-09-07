d6352219d332e4a277393ac2ea93d714cdf99256
wparam <wparam@hotmail.com>
2017-09-07 17:56:00 -0400

    up

--- app/route/core.route.js
+++ app/route/core.route.js
@@ -1,13 +1,9 @@
+const express = require('express');
+const route = express.Router();
+
 module.exports = (app) => {
-    app.get('/', (req, res) => {
+    app.get('/', (req, res, next) => {
         res.render('index', { title: 'AM', message: 'This is a test site'});
     });
-
-    app.all(() => {
-        console.log('hit all');
-    });
-
-    app.get('/test', (req, res) => {
-        console.log('hit htest');
-    });
+   
 }
