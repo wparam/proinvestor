944e778db4484637d76cf50af6577ea30a0df339
wparam <wparam@hotmail.com>
2017-11-08 17:44:41 -0500

    up

--- public/src/containers/full.js
+++ public/src/containers/full.js
@@ -10,27 +10,27 @@ import Footer from '../components/Footer/';
 import Dashboard from '../views/Dashboard';
 
 class Full extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Header />
-        <div className="app-body">
-          <Sidebar {...this.props} />
-          <main className="main">
-            <Breadcrumb />
-            <Container fluid>
-              <Switch>
-                <Route path="/dashboard" name="Dashboard" component={Dashboard} />
-                <Redirect from="/" to="/dashboard" />
-              </Switch>
-            </Container>
-          </main>
-          <Aside />
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <Header />
+                <div className="app-body">
+                    <Sidebar {...this.props} />
+                    <main className="main">
+                        <Breadcrumb />
+                        <Container fluid>
+                            <Switch>
+                                <Route path="/dashboard" name="Dashboard" component={Dashboard} />
+                                <Redirect from="/" to="/dashboard" />
+                            </Switch>
+                        </Container>
+                    </main>
+                    <Aside />
+                </div>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Full;
--- public/src/index.js
+++ public/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-import {createBrowserHistory} from 'history';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 // Styles
 // Import Font Awesome Icons Set
 import 'font-awesome/css/font-awesome.min.css';
-  // Import Simple Line Icons Set
+// Import Simple Line Icons Set
 import 'simple-line-icons/css/simple-line-icons.css';
 // Import Main styles for this application
 import '../resource/scss/style.scss'
@@ -17,11 +17,11 @@ import Full from './containers/full'
 const history = createBrowserHistory();
 
 ReactDOM.render((
-  <HashRouter history={history}>
-    <Switch>
-      <Route path="/" name="Home" component={Full}/>
-    </Switch>
-  </HashRouter>
+    <HashRouter history={history}>
+        <Switch>
+            <Route path="/" name="Home" component={Full} />
+        </Switch>
+    </HashRouter>
 ), document.getElementById('root'));
 
 
