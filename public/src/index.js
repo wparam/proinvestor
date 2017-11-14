60b348f1bc7b839f510553aba5fcc0ff8ab5fbce
wparam@hotmail.com <wparam@hotmail.com>
2017-11-13 22:22:35 -0500

    up

--- public/src/index.js
+++ public/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Root extends React.Component {
-    constructor(){
-        
+    constructor(props){
+        super(props);
+        this.foo = this.foo.bind(this);
     }
     foo() {
         return 'abc';
