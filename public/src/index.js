25fe2c29276d860badb33c64893f4fd49aeaf250
wparam <wparam@hotmail.com>
2017-12-01 18:24:34 -0500

    up

--- public/src/index.js
+++ public/src/index.js
@@ -1,50 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Root extends React.Component {
-    constructor(props, context){
+
+const sd = [
+    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
+
+class Compon extends React.Component {
+    constructor(props, context) {
         super(props);
-        this.foo = this.foo.bind(this);
-        this.state = {title: 'this is a state'};
-    }
-    foo() {
-        return (<SubRoot/>);
+        this.state = {
+            filter: ''
+        };
     }
     render() {
         return (
             <div >
-                {this.foo()}
+                <SearchBox />
             </div>
         );
     }
 }
 
-
-class SubRoot extends React.Component {
-    constructor(props){
+class SearchBox extends React.Component {
+    constructor(props) {
         super(props);
-        this.onClick = this.onClick.bind(this);
         this.state = {
-            cbox: false
+            search: '',
+            showInStock: false
         };
+        this.onTextChange = this.onTextChange.bind(this);
+        this.onTextClick = this.onTextClick.bind(this);
+        this.onCbChange = this.onCbChange.bind(this);
     }
-    onClick(e) {
-        this.setState(pre => ({
-            cbox: !pre.cbox
-        }));
+    onTextChange(e) {
+        this.setState({
+            search : e.target.value
+        });
+    }
+    onCbChange(e){
+        this.setState({
+            showInStock : e.target.value
+        });
+    }
+    onTextClick(){
+        this.setState({
+            search: ''
+        });
     }
     render() {
         return (
-            <div> This is a button <br/>
-                <button onClick={this.onClick} >ClickMe</button>
-                <input type="checkbox" checked={this.state.cbox}/>
+            <div>
+                <div>
+                    <input type="text" value={this.state.search} onChange={this.onTextChange} placeholder="Search..." onClick={this.onTextClick}/>
+                </div>
+                <div>
+                    <input type="checkbox" id="showLabel" onChange={this.onCbChange}/>
+                    <label htmlFor="showLabel">Only show products in stock</label>
+                </div>
             </div>
-            
         );
     }
 }
 
+// class 
+
 ReactDOM.render(
-  <Root name='jason'/>,
-  document.getElementById('root')
+    <Compon />,
+    document.getElementById('root')
 );
