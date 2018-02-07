1a46f638258878554602a9458a05cc012001defe
wparam <wparam@hotmail.com>
2018-02-07 11:19:25 -0500

    restore original src

---
+++ public/demo/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {List, ListItem} from './lists';
+import PropTypes from 'prop-types';
+
+var ds = [
+    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
+
+class Compon extends React.Component {
+    constructor(props, context) {
+        super(props);
+        this.state = {
+            filter: '',
+            showInStock: false,
+            filterList: ds
+        };
+        this.dataSet = ds;
+        this.onFilterChange = this.onFilterChange.bind(this);
+        this.onShowStockChange = this.onShowStockChange.bind(this);
+        this.showPromise = this.showPromise.bind(this);
+        this.filterPromise = this.filterPromise.bind(this);
+        this.filterDataSet = this.filterDataSet.bind(this);
+    }
+    dataSet(){
+        return [
+            { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+            { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+            { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+            { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+            { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+            { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+        ];
+    }
+    showPromise(ds){
+        if(this.state.showInStock)
+            return ds.filter(item => {
+                return item.stocked;
+            });
+        else
+            return ds;
+    }   
+    filterPromise(ds){
+        if(!this.state.filter)
+            return ds;
+        return ds.filter(item => {
+                if(item.name.indexOf(this.state.filter)>=0)
+                    return true;
+                return false;
+            });
+    }
+    filterDataSet(){
+        return this.filterPromise(this.showPromise(this.dataSet));
+    }
+    onFilterChange(e){
+        this.setState({
+            filter : e.target.value
+        });
+        this.filterDataSet();
+    }
+    onShowStockChange(e){
+        this.setState(preState => ({
+            showInStock : !preState.showInStock
+        }));
+        this.filterDataSet();
+    }
+    render() {
+        return (
+            <div > 
+                <SearchBox filter={this.state.filter} onFilterChange={this.onFilterChange} 
+                    onShowStockChange={this.onShowStockChange} showInStock={this.state.showInStock}/>
+                <List stocks={this.filterDataSet()} />
+            </div>
+        );
+    }
+}
+
+class SearchBox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <div title="&lt;3">1</div>
+                <div title="<3">2{'<3'}</div>
+                <div>
+                    <input type="text" value={this.props.filter} onChange={this.props.onFilterChange} 
+                        placeholder="Search..." />
+                </div>
+                <div>
+                    <input type="checkbox" id="showLabel" value={this.props.showInStock} onChange={this.props.onShowStockChange}/>
+                    <label htmlFor="showLabel">Only show products in stock</label>
+                </div>
+            </div>
+        );
+    }
+}
+// class 
+ReactDOM.render(
+    <Compon />,
+    document.getElementById('root')
+);
+
+SearchBox.propTypes = {
+    filter: PropTypes.string
+};
--- public/src/index.js
+++ public/src/index.js
@@ -1,112 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {List, ListItem} from './lists';
-import PropTypes from 'prop-types';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
-var ds = [
-    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
-];
+// Styles
+// Import Font Awesome Icons Set
+import 'font-awesome/css/font-awesome.min.css';
+// Import Simple Line Icons Set
+import 'simple-line-icons/css/simple-line-icons.css';
+// Import Main styles for this application
+import '../resource/scss/style.scss'
 
-class Compon extends React.Component {
-    constructor(props, context) {
-        super(props);
-        this.state = {
-            filter: '',
-            showInStock: false,
-            filterList: ds
-        };
-        this.dataSet = ds;
-        this.onFilterChange = this.onFilterChange.bind(this);
-        this.onShowStockChange = this.onShowStockChange.bind(this);
-        this.showPromise = this.showPromise.bind(this);
-        this.filterPromise = this.filterPromise.bind(this);
-        this.filterDataSet = this.filterDataSet.bind(this);
-    }
-    dataSet(){
-        return [
-            { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-            { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-            { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-            { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-            { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-            { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
-        ];
-    }
-    showPromise(ds){
-        if(this.state.showInStock)
-            return ds.filter(item => {
-                return item.stocked;
-            });
-        else
-            return ds;
-    }   
-    filterPromise(ds){
-        if(!this.state.filter)
-            return ds;
-        return ds.filter(item => {
-                if(item.name.indexOf(this.state.filter)>=0)
-                    return true;
-                return false;
-            });
-    }
-    filterDataSet(){
-        return this.filterPromise(this.showPromise(this.dataSet));
-    }
-    onFilterChange(e){
-        this.setState({
-            filter : e.target.value
-        });
-        this.filterDataSet();
-    }
-    onShowStockChange(e){
-        this.setState(preState => ({
-            showInStock : !preState.showInStock
-        }));
-        this.filterDataSet();
-    }
-    render() {
-        return (
-            <div > 
-                <SearchBox filter={this.state.filter} onFilterChange={this.onFilterChange} 
-                    onShowStockChange={this.onShowStockChange} showInStock={this.state.showInStock}/>
-                <List stocks={this.filterDataSet()} />
-            </div>
-        );
-    }
-}
 
-class SearchBox extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>
-                <div title="&lt;3">1</div>
-                <div title="<3">2{'<3'}</div>
-                <div>
-                    <input type="text" value={this.props.filter} onChange={this.props.onFilterChange} 
-                        placeholder="Search..." />
-                </div>
-                <div>
-                    <input type="checkbox" id="showLabel" value={this.props.showInStock} onChange={this.props.onShowStockChange}/>
-                    <label htmlFor="showLabel">Only show products in stock</label>
-                </div>
-            </div>
-        );
-    }
-}
-// class 
-ReactDOM.render(
-    <Compon />,
-    document.getElementById('root')
-);
+import Full from './containers/full'
 
-SearchBox.propTypes = {
-    filter: PropTypes.string
-};
+const history = createBrowserHistory();
+
+ReactDOM.render((
+    <HashRouter history={history}>
+        <Switch>
+            <Route path="/" name="Home" component={Full} />
+        </Switch>
+    </HashRouter>
+), document.getElementById('root'));
+
+
