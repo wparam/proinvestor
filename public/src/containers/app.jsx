970e66bbad6537d055d1ed7b02120f349f3a387e
Jason <wparam@hotmail.com>
2018-02-21 21:09:51 -0500

    up

--- public/src/containers/app.jsx
+++ public/src/containers/app.jsx
@@ -94,40 +94,39 @@ class App extends Component {
     }
     render() {
         return (
-
-                <div className="wrapper">
-                    <NotificationSystem ref="notificationSystem" style={style}/>
-                    <Sidebar {...this.props} />
-                    <div id="main-panel" className="main-panel">
-                        <Header {...this.props}/>
-                            <Switch>
-                                {
-                                    appRoutes.map((prop,key) => {
-                                        if(prop.name === "Notifications")
-                                            return (
-                                                <Route
-                                                    path={prop.path}
-                                                    key={key}
-                                                    render={routeProps =>
-                                                       <prop.component
-                                                           {...routeProps}
-                                                           handleClick={this.handleNotificationClick}
-                                                       />}
-                                                />
-                                            );
-                                        if(prop.redirect)
-                                            return (
-                                                <Redirect from={prop.path} to={prop.to} key={key}/>
-                                            );
+            <div className="wrapper">
+                <NotificationSystem ref={(noti) => { this.notificationSystem = noti; }} style={style}/>
+                <Sidebar {...this.props} />
+                <div id="main-panel" className="main-panel">
+                    <Header {...this.props}/>
+                        <Switch>
+                            {
+                                appRoutes.map((prop,key) => {
+                                    if(prop.name === "Notifications")
                                         return (
-                                            <Route path={prop.path} component={prop.component} key={key}/>
+                                            <Route
+                                                path={prop.path}
+                                                key={key}
+                                                render={routeProps =>
+                                                    <prop.component
+                                                        {...routeProps}
+                                                        handleClick={this.handleNotificationClick}
+                                                    />}
+                                            />
                                         );
-                                    })
-                                }
-                            </Switch>
-                        <Footer />
-                    </div>
+                                    if(prop.redirect)
+                                        return (
+                                            <Redirect from={prop.path} to={prop.to} key={key}/>
+                                        );
+                                    return (
+                                        <Route path={prop.path} component={prop.component} key={key}/>
+                                    );
+                                })
+                            }
+                        </Switch>
+                    <Footer />
                 </div>
+            </div>
         );
     }
 }
