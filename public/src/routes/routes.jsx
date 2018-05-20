e8c34b685583f59ea9196dccf3bae987996e548f
Jason <wparam@hotmail.com>
2018-05-19 22:32:31 -0400

    up

--- public/src/routes/routes.jsx
+++ public/src/routes/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dashboard from 'views/Dashboard/Dashboard';
+import Overview from 'views/Overview/Overview';
 import UserProfile from 'views/UserProfile/UserProfile';
 import TableList from 'views/TableList/TableList';
 import Typography from 'views/Typography/Typography';
@@ -10,6 +11,7 @@ import Upgrade from 'views/Upgrade/Upgrade';
 
 const appRoutes = [
     { path: '/dashboard', name: 'Dashboard', icon: 'pe-7s-graph', component: Dashboard },
+    { path: '/overview', name: 'Overview', icon: 'pe-7s-display1', component: Overview },
     { path: '/user', name: 'User Profile', icon: 'pe-7s-user', component: UserProfile },
     { path: '/table', name: 'Table List', icon: 'pe-7s-note2', component: TableList },
     { path: '/typography', name: 'Typography', icon: 'pe-7s-news-paper', component: Typography },
---
+++ public/src/views/Overview/Overview.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col, Table } from 'react-bootstrap';
+
+class Overview extends Component {
+    componentDidMount() {
+        console.log('something on mount');
+    }
+    render() {
+        
+
+    }
+}
