42fa7b951c8528f7e93fd9409ba54d23b6815059
Jason <wparam@hotmail.com>
2018-07-01 22:51:17 -0400

    up

--- app/route/data.route.js
+++ app/route/data.route.js
@@ -4,4 +4,7 @@ const authen = require('../controllers/authen.controller')();
 module.exports = (app) => {
     let prefix = '/api/data';
     app.get(prefix + '/*', authen, dataCtrl.proxyDataService);
+
+    let stockPrefix = '/api/stock';
+    app.get(prefix + '/*', authen, dataCtrl.proxyDataService);
 };
