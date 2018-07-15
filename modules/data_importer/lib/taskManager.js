ac0c5a5afe398e5ec41c7419231c601b16e51f30
Jason <wparam@hotmail.com>
2018-07-15 12:58:52 -0400

    update tasks

--- modules/data_importer/lib/taskManager.js
+++ modules/data_importer/lib/taskManager.js
@@ -1,7 +1,33 @@
 module.exports = class TaskManager{
     constructor() {
-        super();
+        this.tasks = new Map(); 
     }
     
+    createTask(taskName, importer){
+        if(this.tasks.has(taskName))
+            return this.tasks.get(taskName);
+        return this.tasks.set(taskName, importer);
+    }
+
+    cancelTask(taskName){   
+        if(!this.tasks.has(taskName))
+            return false;
+        return this.tasks.delete(taskName);
+    }
     
+    async processTasks(){
+        if(this.tasks.keys.length === 0)
+            return Promise.reject(new Error('Tasks list is empty'));
+        return Promise.all(Array.from(this.tasks.values());
+    }
+    
+    /**async function to run task
+     * @param  {} taskName
+     */
+    async runTask(taskName) {
+        if(!taskName || !this.tasks.has(taskName) || !this.tasks.get(taskName)){
+            return Promise.reject(new Error(`Task doest exist: ${taskName}`));
+        }
+        return this.tasks.get(taskName).import();
+    }
 }
