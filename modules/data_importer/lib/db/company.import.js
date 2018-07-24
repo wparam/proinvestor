6765ef90c8bac5c488c840ef3c2b03fc4e6c2d33
wparam <wparam@hotmail.com>
2018-07-24 17:42:45 -0400

    finish load company table

--- modules/data_importer/lib/db/company.import.js
+++ modules/data_importer/lib/db/company.import.js
@@ -1,12 +1,12 @@
 const Importer = require('../importer');
-const https = require('https');
+const http = require('http');
 
 module.exports = class CompanyImporter extends Importer{
     constructor(models, forceMode){
         super(models, forceMode);
         this._modelName = 'company';
         this.model = this.models[this._modelName];
-        this.api = '/stock/{company}/company';
+        this.api = '/api/stock/stock/{company}/company';
         this.mapModel = this.models['m_basket_company'];
     }
     static importerType(){
@@ -21,8 +21,32 @@ module.exports = class CompanyImporter extends Importer{
         this._modelName = v;
     }
 
-    getRemoteData() {
-        
+    filterCompanyData(mapCompanys) {
+        return new Promise((resolve, reject)=>{
+            if(!mapCompanys || mapCompanys.length ===0){
+                return reject(new Error('Fail in prepareCompanyData: Empty data got from mapping table'));
+            }
+            this.model.find({}, 'symbol', function(err, docs){
+                if(err)
+                    return reject(err);
+                let result = mapCompanys.filter((mc)=>{
+                    return !docs.find((c)=>{
+                        return c.symbol===mc;
+                    });
+                });
+                resolve(result);
+            });
+        });
+    }
+
+    getBatchData(batchCompanies){
+        return new Promise((resolve, reject)=>{
+            if(!batchCompanies || batchCompanies.length === 0)
+                return reject(new Error('Fail in getBatchData: Batch load company list emtpy'));
+            Promise.all(batchCompanies.map((c)=>{ return this.getData(c); })).then((result)=>{
+                resolve(result);
+            });
+        });
     }
 
     getData(company){
@@ -30,7 +54,7 @@ module.exports = class CompanyImporter extends Importer{
             if(!company)
                 return reject(new Error('Company is empty'));
             let url = this.api.replace('{company}', company);
-            https.get(url, (res)=>{
+            let req = http.request({ hostname:'localhost', path: url, port:4000 }, (res)=>{
                 if(res.statusCode !== 200)
                     return reject(new Error(`Fail in getData when fetch company infor, company: ${company}`));
                 let s = '';
@@ -39,11 +63,23 @@ module.exports = class CompanyImporter extends Importer{
                     s += chunk;
                 });
                 res.on('end', ()=>{
-                    console.log(s);
                     resolve(JSON.parse(s));
                 });
             }).on('error', (err)=>{
                 reject(err);
+            });
+            req.end();
+        });
+        
+    }
+
+    insertData(data){
+        return new Promise((resolve, reject)=>{
+            let d = data instanceof Array ? data: [data];
+            this.model.insertMany(d, function(err, docs){
+                if(err)
+                    return reject(err);
+                resolve(docs.length);
             });
         });
         
@@ -52,12 +88,15 @@ module.exports = class CompanyImporter extends Importer{
     import() {
         return this.beforeImport().then((d)=>{
             return new Promise((resolve, reject) => {
-                this.mapModel.distinct('company_symbol', {}, function(err, docs){
-                    //tbc
-                    resolve();
+                this.mapModel.distinct('company_symbol', function(err, docs){
+                    resolve(docs);
                 });
             });
-        }).then((d)=>{ console.log('after import'); console.log(d);  })
+        })
+        .then(this.filterCompanyData.bind(this))
+        .then(this.getBatchData.bind(this))
+        .then(this.insertData.bind(this))
+        .then((d)=>{ console.log('after import'); console.log(d);  })
         .catch((err)=>{
             console.log(err.stack);
         }).then(this.afterImport.bind(this));
