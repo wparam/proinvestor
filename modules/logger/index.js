c99a46b56b0d4c3a5fb7fb42b7c163258b512824
Jason <wparam@hotmail.com>
2018-11-24 23:58:32 -0500

    update logger

--- modules/logger/index.js
+++ modules/logger/index.js
@@ -2,7 +2,7 @@ const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 
 const myFormat = printf(info => {
-    return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+    return `${info.timestamp} ${info.level}: ${info.message}`;
   });
 
 const logger = createLogger({
