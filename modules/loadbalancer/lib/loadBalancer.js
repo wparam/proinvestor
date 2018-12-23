232bec40c19230b7829cd72ae8fbc873112c6e76
Jason <wparam@hotmail.com>
2018-12-22 21:24:33 -0500

    up

--- modules/loadbalancer/lib/loadBalancer.js
+++ modules/loadbalancer/lib/loadBalancer.js
@@ -8,10 +8,18 @@ class LoadBalancer{
         this.curPool = new Map();
     }
     getServer(uid){
+        let workingservers = this.getWorkingServers();
+        if(workingservers.length===0){
+            logger.error('LoadBalancer Error: No working servers now');
+            return;
+        }
         let s = this.curPool.get(uid);
         if(this.curPool.has(uid) && this.checkServer(s)){
             return s;
         }
+        let s = workingservers[this.curPool.size % workingservers.length].server;
+        this.curPool.set(uid, s);
+        return s;
     }
     addServer(server, statuspoint){
         let findserver = this.servers.find(s=>s.server === server);
@@ -30,29 +38,37 @@ class LoadBalancer{
         return this.server[serverId] && this.servers[serverId].working;
     }
     start(){
+        //start check servers' status
         
+    }
+    getWorkingServers(){
+        return this.servers.filter(s=>s.working);
     }
     /**
      * async call to check the availability of the service
      */
     isReady(){
-        const checkUrl = this.host + this.statuspoint + this.check;
-        return new Promise((resolve, reject)=>{
-            http.get(checkUrl, res=>{
+        const sp = this.servers.map(s=>new Promise((resolve, reject)=>{
+            let checkUrl = s.server + s.statuspoint;
+            return http.get(checkUrl, res=>{
                 const {statusCode} = res;
                 if(statusCode !== 200){
                     res.resume();
-                    return reject(new Error(`Check Failed on Url: ${checkUrl}`));
+                    return resolve({server: s.server, status: false});
                 }
-                resolve(true);
+                resolve({server: s.server, status: true});
                 res.end();
             }).on('error', (e)=>{
-                reject(e);
+                resolve({server: s.server, status: true});
+            });
+        }));
+        return Promise.all(sp).then(d=>{
+            d.forEach(result=>{
+                let s = this.servers.find(s=>s.server === result.server);
+                s.working = result.status;
             });
         });
     }
-    
-
 }
 
 module.exports = LoadBalancer;
